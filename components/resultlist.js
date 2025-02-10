3180e78fe851a0fdcb23a91c7641cabac112121d
zainmirzaaa <zainnmirzaa@gmail.com>
2025-02-09 19:18:00 -0600

    matched query in results

--- components/resultlist.js
+++ components/resultlist.js
@@ -9,6 +9,20 @@ export default function ResultsList({ results, loading }) {
     return <p className="text-gray-500 mt-4">No results found.</p>;
   }
 
+  function highlight(text, query) {
+  if (!query) return text;
+  const regex = new RegExp(`(${query})`, "gi");
+  return text.split(regex).map((part, i) =>
+    regex.test(part) ? <mark key={i}>{part}</mark> : part
+  );
+}
+
+// inside <li> replace snippet line:
+<p className="text-sm text-gray-600">
+  {highlight(item.snippet, item.query)}
+</p>
+
+
   return (
     <ul className="mt-4 space-y-2">
       {results.map((item, idx) => (
